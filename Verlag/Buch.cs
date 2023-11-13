ae32cc2df58aa58aae57649fccb5144821443f6d
Justin.d22 <justin.d22@gso.schule.koeln>
2023-11-13 12:54:59 +0100

    Fortschritt 13.11.2023

---
+++ Verlag/Buch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Verlag;
+
+public class Buch
+{
+    public static void Main()
+    {
+
+    }
+    private string autor;
+
+    public string Autor
+    {
+        get
+        {
+            return autor;
+        }
+        set
+        {
+            autor = value;
+        }
+    }
+
+    private string titel;
+
+    public string Titel
+    {
+        get
+        {
+            return titel;
+        }
+        set
+        {
+            titel = value;
+        }
+    }
+
+    private int auflage;
+
+    public int Auflage
+    {
+        get
+        {
+            return auflage;
+        }
+        set
+        {
+            auflage = value;
+        }
+    }
+
+    public Buch(string autor, string titel, int auflage)
+    {
+        this.Autor = autor;
+        this.Titel = titel;
+        this.Auflage = auflage;
+
+        if (auflage < 1)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public Buch(string autor, string titel)
+    {
+        this.Autor = autor;
+        this.Titel = titel;
+        this.Auflage = 1;
+    }
+}
